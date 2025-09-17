00c52194f3b7df419afb282b89f9a151d2936676
Rajratna22 <rajratnakamble2022@gmail.com>
2025-09-17 10:59:48 +0000

    comparison

---
+++ javaScript_Basics/04_comparison.js
@@ -0,0 +1,27 @@
+// ** Comparrison in JavaScript
+
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2>=3);
+// console.log(2<=3);
+// console.log(2==3);
+// console.log(2!=3);
+
+
+// console.log("2">1);      // true
+// console.log("02">1);    // true
+
+// console.log(null>0);          // false
+// console.log(null<0);         // false
+// console.log(null<=0);       // true
+// console.log(null>=0);      // true
+// console.log(null==0);     // false
+// console.log(null!==0);   // true
+
+
+
+console.log("2"==2);    // true
+console.log("2"===2);  // false
+
+
+
