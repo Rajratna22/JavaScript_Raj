ce0ee79f95b6fbdb23318eb910c2fe7b260b104f
Rajratna22 <rajratnakamble2022@gmail.com>
2025-09-17 10:17:52 +0000

    conversion

---
+++ javaScript_Basics/03_con_operation.js
@@ -0,0 +1,75 @@
+// ** conversion and Operation in JavaScript
+
+// * Number Conversion
+let score = "raj"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+
+// "44"  => 44
+// "44wder"   => NAN
+// true   => 1 ; false => 0
+//
+
+// * Bollean Conversion
+
+let isLoggedIn = 1                 //=> true
+// isLoggedIn = 0                 //=> false
+// isLoggedIn =""                //=> false
+// isLoggedIn = "raj"           //=> true
+
+let bollenIsLogedIn = Boolean(isLoggedIn)
+//console.log(bollenIsLogedIn);
+
+
+// * String Conversion
+
+let someNumb = 10
+let strNumb = String(someNumb);
+
+//console.log(typeof strNumb)
+
+// ** Operations in JavaScript
+
+let value = 3
+let negValue  = -value
+
+// console.log(negValue)    // => -3
+
+// console.log(2+2)    // => 4
+// console.log(2-2)    // => 0
+// console.log(2*2)    // => 4
+// console.log(2**3)    // => 8
+// console.log(2/2)    // => 1
+// console.log(2%3)    // => 2
+
+// * string Concatination
+
+let str1 = "Hello!"
+let str2 = " Raj"
+
+//console.log(str1 + str2)
+
+// complex Example
+
+// console.log("1"+2)       //=> 12
+// console.log(1+"2")      //=> 12
+// console.log("1"+"2")   //=> 12
+// console.log("1"+2+2)   //=> 122
+// console.log(1+2+"2")   //=> 32
+
+//
+ console.log(true + 1);    // => 1
+
+ 
+
+ 
+
+
+
